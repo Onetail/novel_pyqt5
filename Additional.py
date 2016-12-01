d47205e7154d2e183d8225540e66df678493dde1
Onetail <paosong91@gmail.com>
2016-12-02 00:45:51 +0800

    button have function create

--- Additional.py
+++ Additional.py
@@ -1,11 +1,33 @@
+
 import time
 import FontColor_array
 import requests
 from bs4 import BeautifulSoup
 
-button_id_list = ["add","look","delete","list"]
+# save all list name & get search url 
 
+def addbtn():
+	print("1")
+def lookbtn():
+	print("2")
+def deletebtn():
+	print("3")
+def listbtn():
+	print("4")
+
+button_id_list = ["add","look","delete","list"]
+button_def_list = [addbtn,lookbtn,deletebtn,listbtn]
+
+# get now time
 def Timeget():
 	global stime
-	stime = time.strftime("%Y-%m-%d %H:%M:%S",time.localtime())
+	stime = time.strftime("%Y-%m-%d  %H:%M:%S",time.localtime())
 	return stime
+
+# get url 
+def url_Search():
+	res = requests.get('http://www.appledaily.com.tw/appledaily/hotdaily/'+array[j])
+	res.encoding = 'utf=8'
+	soup = BeautifulSoup(res.text,"html.parser")
+
+
--- FontColor_array.py
+++ FontColor_array.py
@@ -1,4 +1,6 @@
 from PyQt5.QtGui import QFont,QColor
 
+# save main's list word to add Font and color
+
 Font_list = [""]
 Color_list = [""]
