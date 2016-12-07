20ca4225ccc2f0d55fe7c08827b4dc38393cd2e1
Onetail <paosong91@gmail.com>
2016-12-07 21:59:32 +0800

    use to account & passwd

--- FontColor_array.py
+++ FontColor_array.py
@@ -2,5 +2,5 @@ from PyQt5.QtGui import QFont,QColor
 
 # save main's list word to add Font and color
 
-Font_list = [QFont("標楷體",10,5),QFont("細明體",10,15)]
-Color_list = ["background-color: #ffff00"]
+Font_list = [QFont("標楷體",10,10),QFont("細明體",10,15),QFont("微軟正黑體",30,15)]
+Color_list = ["background-color: #ffff00","background-color :#fac091","background-color: #785912"]
--- Login_frame.py
+++ Login_frame.py
@@ -1,5 +1,6 @@
-from PyQt5.QtWidgets import QApplication,QWidget,QPushButton,QLabel,QTextEdit,QVBoxLayout
+from PyQt5.QtWidgets import QApplication,QWidget,QPushButton,QLabel,QTextEdit,QVBoxLayout,QHBoxLayout
 from PyQt5.QtCore import *
+import re
 import sys
 import FontColor_array
 import frame
@@ -14,37 +15,63 @@ class Login(QWidget):
 	def loginui(self):
 		self.resize(600,300)
 		self.setWindowTitle("Login")
+		
 		# button
 		log = QPushButton("登入",self)
 		log.clicked.connect(self.determine)
+
+		log.setFont(FontColor_array.Font_list[2])
 
 		# label
 		title = QLabel("--登入畫面--",self)
 		ac_text = QLabel("帳號: ",self)
 		pw_text = QLabel("密碼: ",self)
 
-		title.setFont(FontColor_array.Font_list[0])
+		title.setFont(FontColor_array.Font_list[2])
+		ac_text.setFont(FontColor_array.Font_list[2])
+		pw_text.setFont(FontColor_array.Font_list[2])
+
+		title.setStyleSheet(FontColor_array.Color_list[1])
 
 		# textEdit
-		account = QTextEdit("",self)
-		passwd = QTextEdit("",self)
+		self.account = QTextEdit("",self)
+		self.passwd = QTextEdit("",self)
 
+		self.account.setFont(FontColor_array.Font_list[2])
+		self.passwd.setFont(FontColor_array.Font_list[2])
+		
 		# layout
-		layout = QVBoxLayout()
-		layout.addWidget(title)
-		layout.addWidget(ac_text)
-		layout.addWidget(pw_text)
-		layout.addWidget(account)
-		layout.addWidget(passwd)
-		layout.addWidget(log)
-		self.setLayout(layout)
+		toplayout = QVBoxLayout()
+		aclayout = QHBoxLayout()
+		pwlayout = QHBoxLayout()
+		mainlayout = QVBoxLayout()
+
+		toplayout.addWidget(title)
+		aclayout.addWidget(ac_text,2)
+		aclayout.addWidget(self.account)
+		pwlayout.addWidget(pw_text,2)
+		pwlayout.addWidget(self.passwd)
+
+		mainlayout.addLayout(toplayout,4)
+		mainlayout.addLayout(aclayout,1)
+		mainlayout.addLayout(pwlayout,1)
+		mainlayout.addWidget(log,2)
+		self.setLayout(mainlayout)
+
 		# show
 		self.show()
+
+	# quit the Application
 	def closeEvent(self,event):
 		self.close_signal.emit()
 		self.close()
+
+	# button Actionlistener
 	def determine(self):
-		self.ui = frame.Application()
-		self.hide()
-		self.close_signal.connect(self.close)
-		self.ui.show()
+		if re.search(self.account.toPlainText(),"[0-9a-zA-Z]{6,7}") != None and re.search(self.passwd.toPlainText,"^[\x30-\x39]$"):
+			self.ui = frame.Application()
+			self.hide()
+			self.close_signal.connect(self.close)
+			self.ui.show()
+		else:
+			return
